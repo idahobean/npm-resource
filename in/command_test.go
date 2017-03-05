9e78780e530c6c99f4a9e284c34184d053b31448
idahobean <idahobean14@gmail.com>
2017-03-05 04:49:41 +0000

    add in command test

--- in/command_test.go
+++ in/command_test.go
@@ -21,3 +21,58 @@ var _ = Describe("In Command", func() {
 	BeforeEach(func() {
 		NPM = &fakes.FakeNPM{}
 		command = in.NewCommand(NPM)
+
+		request = in.Request{
+			Source: resource.Source{
+				Token: "//localhost:4873:_authToken=test",
+				PackageName: "foo",
+				Registry: "http://my.private.registry/",
+			},
+			Params: in.Params{},
+		}
+	})
+
+	Describe("running the command", func() {
+		It("pulls package", func() {
+			response, err := command.Run(request)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			Ω(response.Version.Version).Should(Equal("0.0.1"))
+			Ω(reqponse.Metadata[0]).Should(Equal(
+				resource.MetadataPair{
+					Name: "name",
+					Value: "foo-package",
+				},
+			))
+			Ω(response.Metadata[1]).Should(Equal(
+				resource.MetadataPair{
+					Name: "homepage",
+					Value: "foobars page",
+				},
+			))
+
+			By("npm install")
+			Ω(NPM.InstallCallCount()).Should(Equal(1))
+
+			packageName, registry := NPM.InstallArgsForCall(0)
+			Ω(packageName).Should(Equal("foo-package"))
+			Ω(registry).Should(Equal("http://my.private.registry/"))
+
+		})
+
+		Describe("handling any errors", func() {
+			var expectedError error
+
+			BeforeEach(func() {
+				expectedError = errors.New("it all went wrong")
+			})
+
+			It("from install package", func() {
+				NPM.InstallReturns(expectedError)
+
+				_, err := command.Run(request)
+				Ω(err).Should(MatchError(expectedError))
+			})
+		})
+	})
+})
