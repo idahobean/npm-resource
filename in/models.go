50b519efc47e665adc0d6bb4dd57b016f36279a6
idahobean <idahobean14@gmail.com>
2017-03-18 06:00:18 +0000

    delete unused version

--- in/models.go
+++ in/models.go
@@ -4,7 +4,6 @@ import "github.com/idahobean/npm-resource"
 
 type Request struct {
 	Source  resource.Source  `json:"source"`
-	Version resource.Version `json:"version"`
 }
 
 type Response struct {
