c52defabe0878b8b3a34694b9fb2d52c457d83f9
idahobean <idahobean14@gmail.com>
2017-06-03 10:25:51 +0000

    fix errors

--- out/integration_test.go
+++ out/integration_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"os/exec"
 	"path/filepath"
 
@@ -86,7 +87,7 @@ var _ = Describe("Out", func() {
 
 				// shim outputs arguments
 				Ω(session.Err).Should(gbytes.Say("npm-cli-login -u abc -p def -e ghi@jkl.mno -r http://localhost:8080/"))
-				Ω(session.Err).Should(gbytes.Say("npm publish %s --tag stable --registry http://localhost:8080/", packagePath))
+				Ω(session.Err).Should(gbytes.Say("npm publish sample-node --tag stable --registry http://localhost:8080/"))
 				Ω(session.Err).Should(gbytes.Say("npm view sample-node --registry http://localhost:8080/"))
 				Ω(session.Err).Should(gbytes.Say("npm logout --registry http://localhost:8080/"))
 			})
